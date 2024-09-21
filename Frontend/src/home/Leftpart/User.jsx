9617d356e339a5f0b0590458577dc5fdf7a9d198
Lavkush-vishwa <kush88104329@gmail.com>
2024-09-21 16:37:49 +0530

    this full responsive chat application

--- Frontend/src/home/Leftpart/User.jsx
+++ Frontend/src/home/Leftpart/User.jsx
@@ -11,12 +11,12 @@ function User({ user }) {
   const isOnline = onlineUsers.includes(user._id);
   return (
     <div
-      className={`hover:bg-slate-600 duration-300 ${
+      className={`hover:bg-green-600 duration-300 ${
         isSelected ? "bg-slate-700" : ""
       }`}
       onClick={() => setSelectedConversation(user)}
     >
-      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+      <div className="flex space-x-4 px-8 py-3 hover:bg-green-700 duration-300 cursor-pointer">
         <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={profile} />
